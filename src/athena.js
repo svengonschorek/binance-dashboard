8ec1d5036fcc57029835d0c8ca2242737f55174f
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2023-02-20 17:47:13 +0100

    pl: add profit and loss

--- src/athena.js
+++ src/athena.js
@@ -77,3 +77,45 @@ export async function getWalletAmount() {
     return data
 
 }
+
+export async function getProfitLoss() {
+    let myQuery = {
+        sql: `
+            with base as (
+                select
+                    cast(from_unixtime(cast(ts/1000 AS bigint)) as date) as balance_on,
+                    sum(amount_eur) as amount
+                from raw_data.daily_wallet_export
+                group by cast(from_unixtime(cast(ts/1000 AS bigint)) as date)
+            ),
+            daily_pl as (
+                select
+                    balance_on,
+                    date_format(balance_on, '%M') as balance_month,
+                    date_format(balance_on, '%Y') as balance_year,
+                    lag(amount, 1) over (order by balance_on desc) - amount as daily_pl
+                from base
+                order by balance_on desc
+            )
+            select
+                balance_on,
+                daily_pl,
+                sum(daily_pl) over (partition by balance_month, balance_year) as monthly_pl,
+                sum(daily_pl) over (partition by balance_year) as yearly_pl
+            from daily_pl
+            where daily_pl is not null
+            limit 1
+        `,
+        db: "raw_data",
+        pagination: 1
+    }
+
+    let data = [];
+    response = await athenaExpress.query(myQuery);
+    
+    for (var i = 0; i < response.Items.length; i++) {
+        data.push(response.Items[i])
+    }
+
+    return data
+}
